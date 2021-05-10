bcc052fbebfa45b4e9a8a1c57fd7f5d80f1fb1c1
Romualdus Aldi Hermanto <romheraldi1@gmail.com>
2021-05-10 23:39:29 +0700

    4.2

--- locker.ino
+++ locker.ino
@@ -19,7 +19,7 @@ const char *mqtt_server = "mqtt.rupira.com";
 #define NUM_LEDS 1
 #define DATA_PIN 2
 
-const String FirmwareVer = {"4.1"};
+const String FirmwareVer = {"4.2"};
 #define URL_fw_Version "https://raw.githubusercontent.com/Sthira-Nusantara/iot-locker-firmware/master/version.txt"
 #define URL_fw_Bin "https://raw.githubusercontent.com/Sthira-Nusantara/iot-locker-firmware/master/firmware.bin"
 
@@ -350,77 +350,38 @@ void callback(char *topic, byte *payload, unsigned int length)
     }
   }
 
-  if (String(topic).indexOf(toggle) >= 0)
+
+  if (String(topic).indexOf(successSubs) >= 0)
   {
     if (payload[0] == '1')
     {
-      deviceClient.setInsecure();
-      HTTPClient https;
+      digitalWrite(RELAY, LOW);
+      Serial.println("Door Open");
+      changeColor(CRGB::Green);
 
-      https.begin(deviceClient, URL_authenticate.c_str()); //HTTP
-      https.addHeader("Content-Type", "application/json");
+      delay(5000);
 
-      Serial.println("Authenticating...");
-      int httpCode = https.POST("{\"request\": \"" + String(topic) + "\"}");
+      // Close Door
+      digitalWrite(RELAY, HIGH);
+      Serial.println("Door Close");
+      changeColor(CRGB::Red);
 
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] POST... code: %d\n", httpCode);
-
-        // file found at server
-        if (httpCode == HTTP_CODE_OK)
-        {
-          const String& payload = https.getString();
-          Serial.println("received payload:\n<<");
-          Serial.println(payload);
-          Serial.println(">>");
-
-
-          DynamicJsonDocument doc(1024);
-
-          deserializeJson(doc, payload);
-
-          const char* hasil = doc["data"];
-
-          if (String(hasil) == "true")
-          {
-            // Open Door
-            digitalWrite(RELAY, HIGH);
-            Serial.println("Door Open");
-            changeColor(CRGB::Green);
-
-            delay(5000);
-
-            // Close Door
-            digitalWrite(RELAY, LOW);
-            Serial.println("Door Close");
-            changeColor(CRGB::Red);
-
-
-            delay(1000);
-          }
-        }
-        else
-        {
-          const String& payload = https.getString();
-          Serial.println("error payload:\n<<");
-          Serial.println(payload);
-          Serial.println(">>");
-
-          Serial.println("Sorry, you are not authenticated");
-
-          blinkingColor(CRGB::Red, 3, 250);
-
-          delay(1000);
-
-        }
-      } else {
-        Serial.printf("[HTTP] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
-      }
-
-      //      https.end();
+      delay(1000);
     }
   }
+
+  if (String(topic).indexOf(failureSubs) >= 0)
+  {
+    if (payload[0] == '1')
+    {
+      Serial.println("Sorry, you are not authenticated");
+
+      blinkingColor(CRGB::Red, 3, 250);
+
+      delay(1000);
+    }
+  }
+
 
   Serial.println();
   //  return;
@@ -444,7 +405,7 @@ boolean reconnect()
     //      Your Subs
     mqttClient.subscribe(successSubs.c_str());
     mqttClient.subscribe(failureSubs.c_str());
-    mqttClient.subscribe(openSubs.c_str());
+//    mqttClient.subscribe(openSubs.c_str());
     mqttClient.subscribe(testSubs.c_str());
     digitalWrite(RELAY, LOW);
   }
